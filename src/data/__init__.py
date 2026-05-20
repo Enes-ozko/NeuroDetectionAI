22aab1112202c7cbda459bf3c59d9830a4adb502
Hugo <munarettoh@gmail.com>
2026-05-20 09:56:55 +0200

    remise à plat du projet

--- src/data/__init__.py
+++
@@ -1,4 +0,0 @@
-from .dataset import BrainTumorDataset, collect_data
-from .transforms import get_transforms
-from .dataloader import build_folds
-from .visualize import plot_fold_distribution
--- src/etape2/__init__.py
+++
@@ -1,3 +0,0 @@
-from .model    import get_binary_model
-from .train    import train_etape2
-from .evaluate import evaluate_etape2
